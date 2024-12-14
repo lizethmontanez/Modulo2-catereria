c1aa1ec0785d55b049d7797c826d1b7589fa4038
Lizeth <lizeth.mcx@gmail.com>
2024-12-14 16:19:40 -0600

    Se agregó video y diseños responsivos en splides

--- main.js
+++ main.js
@@ -2,15 +2,40 @@ function redirigir (url){
     window.location.href = url;
 }
 
+//Galeria-bestsellers
+new Splide('#galeria', {
+    perPage: 4,            
+    perMove: 1,            
+    breakpoints: {
+    780: {               
+        perPage: 2,        
+    },
+    },
+}).mount();
 
-//Galeria
-new Splide( '#galeria', {
-    perPage: 4,
-    perMove: 1
-} ).mount();
+//Galeria-experiencias
+new Splide('#galeria-2', {
+    perPage: 2,            
+    perMove: 1,            
+    breakpoints: {
+    780: {               
+        perPage: 1,        
+    },
+    },
+}).mount();
 
-//Galeria
-new Splide( '#galeria-2', {
-    perPage: 2,
-    perMove: 1
-} ).mount();
+
+//Obtener el header
+const header = document.querySelector('header');
+const logo = document.querySelector('#logo img');
+
+//Escuchar el evento de scroll
+window.addEventListener('scroll', function () {
+    if (window.scrollY > window.innerHeight) {
+        header.className = 'scrolled';
+        logo.src = "img-friends/logo central perk.webp";
+    } else {
+        header.className = '';
+        logo.src = "img-friends/logoverde.jpg";
+    }
+});
